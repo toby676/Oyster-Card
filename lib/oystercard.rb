951dce2caecb04dd640be4ac8511280bde043637
Andy <andy.bell.github@gmail.com>
2016-05-30 17:22:29 +0100

    completed step 7, R/G/R on deduct method completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -10,14 +10,30 @@ class Oystercard
     top_up_fail(value)
     @balance += value
   end
+  
+  def deduct(value)
+    fail "Please input an integer" unless is_number?(value)
+    fail "Insufficient funds" if (@balance - value) < 0
+    @balance -= value
+  end
 
   private
 
   DEFAULT_LIMIT = 90
-  
+  DEFAULT_MIN = 0
+
   def top_up_fail(value)
     fail 'Please input an integer' unless is_number?(value)
     fail 'Exceeded limit' if limit?(value)
+  end
+  
+  def deduct_fail(value)
+    fail "Please input an integer" unless is_number?(value)
+    fail "Insufficient funds" if empty?(value)
+  end
+
+  def empty?(value)
+    (@balance - value) < DEFAULT_MIN
   end
 
   def limit?(value)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,7 +4,8 @@ describe Oystercard do
   let(:card) { described_class.new } 
   context 'responses' do
     it { is_expected.to respond_to :balance }
-    it { is_expected.to respond_to :top_up }
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it { is_expected.to respond_to(:deduct).with(1).argument }
   end
 
   context '#top_up' do
@@ -32,6 +33,32 @@ describe Oystercard do
     it 'raises an error when exceeding limit of 90' do
       card.top_up(90)
       expect{card.top_up(1)}.to raise_error("Exceeded limit")
+    end
+  end
+
+  context '#deduct' do
+    before(:each) do
+      card.top_up(10)
+    end
+    
+    it 'deducts from the card' do
+      expect{card.deduct(0)}.not_to raise_error
+    end
+    
+    it 'deducts 10 from the card' do
+      expect(card.deduct(10)).to eq 0
+    end
+
+    it 'deducts 5 from the card' do
+      expect(card.deduct(5)).to eq 5
+    end
+
+    it 'raises an error if not an integer' do
+      expect{card.deduct("foo")}.to raise_error("Please input an integer")
+    end
+    
+    it 'raises an error if balance would go below zero' do
+      expect{card.deduct(11)}.to raise_error("Insufficient funds")
     end
   end
 
