df6f21e58709ac2cc0152ed3449f2a5bb57ee70b
srMarquinho <sr.marquinho@gmail.com>
2016-05-30 16:13:22 +0100

    add touch methods

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,11 +6,28 @@ class OysterCard
 
 	def initialize
 		@balance = 0
+    @in_journey = false
 	end
 
   def top_up(amount)
   	fail "Maximum balance of #{MAXIMUM_BALANCE} has been reached." if (amount + @balance) > MAXIMUM_BALANCE
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -20,6 +20,38 @@ describe OysterCard do
     	subject.top_up(maximum_balance)
     	expect{subject.top_up(1)}.to raise_error "Maximum balance of #{maximum_balance} has been reached."
     end
-
   end
+
+  context '#deduct' do
+    it 'responds to deduct method' do
+      expect(subject).to respond_to(:deduct).with(1).argument
+    end
+
+    it 'deducts balance by requested amount' do
+      subject.top_up(10)
+      expect{subject.deduct(2)}.to change{subject.balance}.by -2
+    end
+  end
+
+  context '#in_journey?' do
+    it 'initializes with false' do
+      expect(subject).not_to be_in_journey
+    end
+  end
+
+  context '#touch_in' do
+    it 'changes in_journey to false' do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+  context '#touch_out' do
+    it 'changes in_journey to true' do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
+
 end
