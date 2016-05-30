0213a7a135fdbe7ee34d1e08e64c959a623c6d5f
Andy <andy.bell.github@gmail.com>
2016-05-30 17:03:15 +0100

    Completed steps 5 & 6, adding top-up functionality and imposing a limit on card balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -5,4 +5,26 @@ class Oystercard
   def initialize
     @balance = 0
   end
+  
+  def top_up(value)
+    top_up_fail(value)
+    @balance += value
+  end
+
+  private
+
+  DEFAULT_LIMIT = 90
+  
+  def top_up_fail(value)
+    fail 'Please input an integer' unless is_number?(value)
+    fail 'Exceeded limit' if limit?(value)
+  end
+
+  def limit?(value)
+    (@balance + value ) > DEFAULT_LIMIT
+  end
+
+  def is_number?(value)
+    value.is_a? Integer
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,14 +1,45 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:card) { described_class.new } 
   context 'responses' do
     it { is_expected.to respond_to :balance }
+    it { is_expected.to respond_to :top_up }
   end
 
-  context 'balance' do
+  context '#top_up' do
+    it 'adds 10 to the card' do
+      expect(card.top_up(10)).to eq 10
+    end
+
+    it 'adds 5 to the card' do
+      expect(card.top_up(5)).to eq 5
+    end
+
+    it 'adds 5 to a card with 10 already' do
+      card.top_up(10)
+      expect(card.top_up(5)).to eq 15
+    end
+
+    it 'raises an error when a string is input' do
+      expect{card.top_up("foo")}.to raise_error("Please input an integer")
+    end
+
+    it 'allows money to be added to reach limit' do
+      expect{card.top_up(90)}.not_to raise_error
+    end
+
+    it 'raises an error when exceeding limit of 90' do
+      card.top_up(90)
+      expect{card.top_up(1)}.to raise_error("Exceeded limit")
+    end
+  end
+
+  context ':balance' do
     it "has a balance of 0" do
       card = Oystercard.new
       expect(card.balance).to eq 0
     end
   end
+
 end
