f83230753d742f54d02106374608dcbc7d08174d
Faisal Mahmood <fahmahmood@gmail.com>
2016-05-31 18:00:43 +0100

    end of day 2

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,17 @@
 class Oystercard
  	
+ MAXIMUM_BALANCE = 90
+
  attr_reader :balance
- 
+
+
  def initialize
   		@balance = 0
   end
 
   def top_up(amount)
-  	@balance = @balance + amount
+  	fail 'Maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
+  	@balance += amount
   end 
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -12,7 +12,14 @@ describe '#top_up' do
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
   it 'can top up the balance' do
-    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+    expect{subject.top_up 1}.to change{subject.balance}.by 1
+  end
+
+
+  it 'raises an error if the maximum balance is exceeded' do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    subject.top_up(maximum_balance)
+    expect{ subject.top_up 1 }.to raise_error 'Maximum balance exceeded'
   end
 
 end
