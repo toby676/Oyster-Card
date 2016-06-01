d16951237c7ee1d5cc734626fc9ebaba9dcb3f62
Faisal Mahmood <fahmahmood@gmail.com>
2016-06-01 11:24:50 +0100

    joining jojo

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,26 +2,36 @@ require 'oystercard'
 
 describe Oystercard do
   
- it 'has an initial balance of 0' do
+ 	it 'has an initial balance of 0' do
     expect(subject.balance).to eq(0)
-  end
+  	end
 
 
 describe '#top_up' do
 
-  it { is_expected.to respond_to(:top_up).with(1).argument }
+  	it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  it 'can top up the balance' do
+  	it 'can top up the balance' do
     expect{subject.top_up 1}.to change{subject.balance}.by 1
-  end
+  	end
 
 
-  it 'raises an error if the maximum balance is exceeded' do
+  	it 'raises an error if the maximum balance is exceeded' do
     maximum_balance = Oystercard::MAXIMUM_BALANCE
     subject.top_up(maximum_balance)
     expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
-  end
+  	end
 
 end
+
+describe '#deduct' do
+
+	it { is_expected.to respond_to(:deduct).with(1).argument }
+
+	it 'deducts a fare to pay for journey' do
+	expect{subject.deduct -1 }.to change{subject.balance}.by -1 
+	end 
+
+end 
 
 end 
