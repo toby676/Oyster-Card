739f1f7ad42bb82265a1f4f05a6ba3e11e46b729
Jonathan <j_passe@live.fr>
2016-06-01 12:36:05 +0100

    added in_journey and touch_in method, red - green

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,5 @@
 class Oystercard
- 	
+
  MAXIMUM_BALANCE = 90
 
  attr_reader :balance
@@ -7,12 +7,25 @@ class Oystercard
 
  def initialize
   		@balance = 0
+      @journey = false
   end
 
   def top_up(amount)
   	fail 'Maximum balance of #{maximum_balance} exceeded' if amount + balance > MAXIMUM_BALANCE
   	@balance += amount
-  end 
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @journey
+  end
+
+  def touch_in
+    @journey = true
+  end
 
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,37 +1,51 @@
 require 'oystercard'
 
 describe Oystercard do
-  
- 	it 'has an initial balance of 0' do
+
+ 	  it 'has an initial balance of 0' do
     expect(subject.balance).to eq(0)
   	end
 
 
-describe '#top_up' do
+  describe '#top_up' do
 
-  	it { is_expected.to respond_to(:top_up).with(1).argument }
+    	it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  	it 'can top up the balance' do
-    expect{subject.top_up 1}.to change{subject.balance}.by 1
-  	end
+    	it 'can top up the balance' do
+      expect{subject.top_up 1}.to change{subject.balance}.by 1
+    	end
 
 
-  	it 'raises an error if the maximum balance is exceeded' do
-    maximum_balance = Oystercard::MAXIMUM_BALANCE
-    subject.top_up(maximum_balance)
-    expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
-  	end
+    	it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up 1 }.to raise_error 'Maximum balance of #{maximum_balance} exceeded'
+    	end
+
+  end
+
+  describe '#deduct' do
+
+    	it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    	it 'deducts a fare to pay for journey' do
+    	expect{subject.deduct 1 }.to change{subject.balance}.by -1
+    	end
+  end
+
+  describe '#in_journey?' do
+      it {is_expected.to respond_to(:in_journey?)}
+
+      it 'initializes journey with false' do
+        expect(subject.in_journey?).to be(false)
+      end
+
+      it 'responds to touch_in with true' do
+        subject.in_journey?
+        subject.touch_in
+        expect(subject.in_journey?).to be true
+      end
+
+  end
 
 end
-
-describe '#deduct' do
-
-	it { is_expected.to respond_to(:deduct).with(1).argument }
-
-	it 'deducts a fare to pay for journey' do
-	expect{subject.deduct -1 }.to change{subject.balance}.by -1 
-	end 
-
-end 
-
-end 
